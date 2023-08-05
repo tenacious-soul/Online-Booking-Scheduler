8ff9cc201b129642cd330f1c5b3c0f13fd7fb532
tenacious_soul <simsri2805@gmail.com>
2023-08-05 14:31:40 +0530

    Intitialsed project structure

---
+++ src/main/java/com/volt/bookingScheduler/BookingSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.volt.bookingScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volt/bookingScheduler/BookingSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volt.bookingScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
